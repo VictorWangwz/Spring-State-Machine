751ddcb0e5b5b91a046cfb8ca56f8710f0e4f80c
VictorWangwz <wangzhen5161@outlook.com>
2019-07-05 17:40:09 +0800

    init commit

---
+++ src/main/java/com/victorzhenwang/statemachinedemo/StatemachinedemoApplication.java
@@ -0,0 +1,13 @@
+package com.victorzhenwang.statemachinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatemachinedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatemachinedemoApplication.class, args);
+	}
+
+}
